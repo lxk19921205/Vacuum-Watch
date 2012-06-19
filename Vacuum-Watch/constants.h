6b5a555849a9f8491fb88640d5c6e9e39eb1ae87
Andriy Lin <lxk19921205@gmail.com>
2012-06-20 00:21:47 +0800

    add some directories to .gitignore; this is the first application that can run, although just a small blue window with nothing inside

---
+++ Vacuum-Watch/constants.h
@@ -0,0 +1,6 @@
+#ifndef __VACUUM_WATCH_CONSTANTS_H
+#define __VACUUM_WATCH_CONSTANTS_H
+
+#define VW_WINDOW_TITLE ("Vacuum-Watch")
+
+#endif
---
+++ Vacuum-Watch/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <gl/glut.h>
+
+#include "constants.h"
+
+using namespace std;
+
+
+void RenderScene()
+{
+	//用当前清除颜色清除窗口
+	glClear(GL_COLOR_BUFFER_BIT);
+	//刷新绘图命令
+	glFlush();
+}
+
+void SetupRC()
+{
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+}
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutCreateWindow(VW_WINDOW_TITLE);
+	glutDisplayFunc(RenderScene);
+
+	SetupRC();
+	glutMainLoop();
+
+	return 0;
+}
