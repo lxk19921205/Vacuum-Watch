e5c80036f3c28480ae16dd94eb0a6387ec01f671
rilyhong <492236516@qq.com>
2012-06-21 20:29:43 +0800

    option

--- Vacuum-Watch/main.cpp
+++ Vacuum-Watch/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <gl/glut.h>
-
+//#include <gl/glut.h>
+#include "COption.h"
 #include "constants.h"
 #include "module_interfaces.h"
 #include "factory.h"
@@ -8,23 +8,45 @@
 using std::cout;
 using std::endl;
 
-
-void RenderScene()
+//
+//void RenderScene()
+//{
+//	//用当前清除颜色清除窗口
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	//刷新绘图命令
+//	glFlush();
+//}
+//
+//void SetupRC()
+//{
+//	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+//}
+//
+void SaveOption()
 {
-	//用当前清除颜色清除窗口
-	glClear(GL_COLOR_BUFFER_BIT);
-	//刷新绘图命令
-	glFlush();
+	COption *op=new COption();
+	op->SaveIntoFile();
 }
-
-void SetupRC()
+void RestoreOption()
 {
-	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	COption *op=new COption();
+	op->RestoreFromFile();
 }
-
+void SetKey()
+{
+	COption * op=new COption();
+	op->SetFireKey(65);
+	op->SaveIntoFile();
+}
+void GetKey()
+{
+	COption* op=new COption();
+	op->RestoreFromFile();
+	cout<<op->GetFireKey();
+}
 
 int main(int argc, char** argv)
-{
+{/*
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 	glutCreateWindow(VW_WINDOW_TITLE);
@@ -33,5 +55,9 @@ int main(int argc, char** argv)
 	SetupRC();
 	glutMainLoop();
 
-	return 0;
+	return 0;*/
+//	SaveOption();
+	//RestoreOption();
+//SetKey();
+//	GetKey();
 }
